e1fd41ed84c1f1862b838e29b9ff753a052f4f87
nitramiz <martin@blockstream.com>
2019-08-12 16:13:37 -0700

    update gcloud-docker + make blc opportunistic

--- terraform/modules/blc/main.tf
+++ terraform/modules/blc/main.tf
@@ -28,7 +28,7 @@ resource "google_compute_instance_group_manager" "blc" {
   }
 
   update_policy {
-    type                  = "PROACTIVE"
+    type                  = "OPPORTUNISTIC"
     minimal_action        = "REPLACE"
     max_surge_fixed       = 0
     max_unavailable_fixed = 1

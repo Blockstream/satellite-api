19a5e08e91108be6a648886e99f2e6537e7a6bf4
nitramiz <martin@blockstream.com>
2021-07-21 10:34:46 -0700

    CI: add build job for tags; TF: cleanup

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -3,12 +3,12 @@ resource "google_service_account" "api_server_ci" {
   account_id   = "satellite-api-tf-ci"
   display_name = "satellite-api-tf-ci"
   description  = "Terraform/CI"
-  count        = local.create_mainnet
+  count        = local.env == "prod" ? local.create_mainnet : 0
 }
 
 resource "google_project_iam_member" "api_server_ci" {
   project = var.project
   role    = "roles/editor"
   member  = "serviceAccount:${google_service_account.api_server_ci[0].email}"
-  count   = local.create_mainnet
+  count   = local.env == "prod" ? local.create_mainnet : 0
 }

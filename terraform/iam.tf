c68b6e053cf4e3efbb136af53356405f7293ace0
nitramiz <martin@blockstream.com>
2023-05-22 17:31:57 +0000

    TF: Assign CI svc account storage.admin

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -3,12 +3,19 @@ resource "google_service_account" "api_server_ci" {
   account_id   = "satellite-api-tf-ci"
   display_name = "satellite-api-tf-ci"
   description  = "Terraform/CI"
-  count        = local.env == "prod" ? local.create_mainnet : 0
+  count        = local.create_misc
 }
 
 resource "google_project_iam_member" "api_server_ci" {
   project = var.project
   role    = "roles/editor"
   member  = "serviceAccount:${google_service_account.api_server_ci[0].email}"
-  count   = local.env == "prod" ? local.create_mainnet : 0
+  count   = local.create_misc
+}
+
+resource "google_project_iam_member" "api_server_ci_storageadm" {
+  project = var.project
+  role    = "roles/storage.admin"
+  member  = "serviceAccount:${google_service_account.api_server_ci[0].email}"
+  count   = local.create_misc
 }
